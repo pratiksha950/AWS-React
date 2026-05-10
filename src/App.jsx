1e2310f04b128b82229fb348ff0433a493355eea
pratiksha950 <salunkepratiksha950@gmail.com>
2026-05-10 10:03:44 +0530

    yml file added:

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      
+      <h1>React App Deployment</h1>
       <h2>Aws Hosting</h2>
       <p>Hosting your React app on AWS is a great way to ensure scalability, reliability, and global accessibility. AWS offers various services that can be used to host your React application, such as Amazon S3 for static website hosting, Amazon CloudFront for content delivery, and AWS Amplify for a more streamlined deployment process.</p>
     </>
